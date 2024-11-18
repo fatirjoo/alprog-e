55c9909ac45e89a536e7bb7b3b2e87e9ac0b7f78
fatirjoo <mfathiralfariz87@gmail.com>
2024-11-18 16:39:56 +0800

    Add files via upload
    Signed-off-by: fatirjoo <mfathiralfariz87@gmail.com>

---
+++ tugas7.py
@@ -0,0 +1,9 @@
+#menghitung segitiga
+#nama : M.Fathir al fariz
+
+print("menghitung segitiga")
+alas = int(input("masukan tinggi : "))
+tinggi = int(input("masukan tinggi : "))
+
+hasil = 0.5 * alas * tinggi
+print(f"hasil dari120 x {alas}x{tinggi}adalah : {hasil}")
