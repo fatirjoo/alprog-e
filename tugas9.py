4aa288d39322fac80af1d45d25beebd4893648d5
fatirjoo <mfathiralfariz87@gmail.com>
2024-12-06 10:01:14 +0800

    Add files via upload
    Signed-off-by: fatirjoo <mfathiralfariz87@gmail.com>

---
+++ tugas9.py
@@ -0,0 +1,13 @@
+#latihan operasi aritmatika
+
+
+nilai = float(input("masukkan nilai suhu dalam celcius :"))
+
+fahrenheit = (nilai * 9/5)+32
+print("nilai dalam fahrenheit =",fahrenheit)
+
+kelvin= nilai +273,151
+print("nilai dalam kelvin =",kelvin)
+
+reamur = nilai*(5/4)+0
+print("nilai dalam reamur=",reamur)
